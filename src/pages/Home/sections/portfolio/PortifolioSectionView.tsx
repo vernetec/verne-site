18c05e8ea824f4cc9e31d2a652b2531d71ad2a02
Raniere Araujo <ranierepsa@gmail.com>
2026-03-08 14:58:25 +0000

    feat: Atualizado imagem de portifólio

--- src/pages/Home/sections/portfolio/PortifolioSectionView.tsx
+++ src/pages/Home/sections/portfolio/PortifolioSectionView.tsx
@@ -1,20 +1,21 @@
 import { Container } from '@/components/ui';
-import portfolioImage from '@/assets/portifolio.png';
+import portfolioImage from '@/assets/portifolio2.png';
 import styles from './PortifolioSectionView.module.css';
 
 function PortifolioSectionView() {
   return (
     <section id="portifolio" className={styles.portfolio}>
-      <img
-        src={portfolioImage}
-        alt="Portifólio"
-        className={styles.bgImage}
-      />
       <Container className={styles.container}>
         <div className={styles.content}>
           <div>
             <h2 className={styles.title}>Portfólio</h2>
           </div>
+
+        <img
+          src={portfolioImage}
+          alt="Portifólio"
+          className={styles.bgImage}
+        />
         </div>
       </Container>
     </section>
